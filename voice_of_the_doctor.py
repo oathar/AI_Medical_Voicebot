cad7e2e06f58bc2df8ab5d3906e69c660e98f293
Athar Ramzan <atharramzan.ofi@gmail.com>
2026-01-15 00:13:06 +0530

    added autoplay feature (text ---> voice)using gtts and elevenlabs

--- voice_of_the_doctor.py
+++ voice_of_the_doctor.py
@@ -14,13 +14,52 @@ def text_to_speech_with_gtts_old(input_text, output_filepath):
 
 
 input_text="Hi, this is a test of text to speech conversion using gTTS and ElevenLabs."
-text_to_speech_with_gtts_old(input_text=input_text, output_filepath="gtts_testing.mp3")
+# text_to_speech_with_gtts_old(input_text=input_text, output_filepath="gtts_testing.mp3")
 
 #Step1b: Setup Text to Speech–TTS–model with ElevenLabs
 import elevenlabs
 from elevenlabs.client import ElevenLabs
 
 ELEVENLABS_API_KEY=os.environ.get("ELEVENLABS_API_KEY")
+
+def text_to_speech_with_elevenlabs_old(input_text, output_filepath):
+    client=ElevenLabs(api_key=ELEVENLABS_API_KEY)
+    audio=client.text_to_speech.convert(
+        text= input_text,
+        voice_id= "21m00Tcm4TlvDq8ikWAM",
+        output_format= "mp3_22050_32",
+        model_id= "eleven_turbo_v2_5"
+    )
+    elevenlabs.save(audio, output_filepath)
+
+# text_to_speech_with_elevenlabs_old(input_text, output_filepath="elevenlabs_testing.mp3")
+
+
+#Step2: Use Model for Text output to Voice (autoplay)
+import subprocess
+import platform
+import pygame
+
+def text_to_speech_with_gtts(input_text, output_filepath):
+    language="en"
+
+    audioobj= gTTS(
+        text=input_text,
+        lang=language,
+        slow=False
+    )
+    audioobj.save(output_filepath)
+    # Autoplay the audio
+    pygame.mixer.init()
+    pygame.mixer.music.load(output_filepath)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pygame.time.wait(100)
+
+
+input_text="Hi this is Ai with Athar, autoplay testing with gTTS and ElevenLabs."
+# text_to_speech_with_gtts(input_text=input_text, output_filepath="gtts_testing_autoplay.mp3")
+
 
 def text_to_speech_with_elevenlabs(input_text, output_filepath):
     client=ElevenLabs(api_key=ELEVENLABS_API_KEY)
@@ -31,5 +70,11 @@ def text_to_speech_with_elevenlabs(input_text, output_filepath):
         model_id= "eleven_turbo_v2_5"
     )
     elevenlabs.save(audio, output_filepath)
+    # Autoplay the audio
+    pygame.mixer.init()
+    pygame.mixer.music.load(output_filepath)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pygame.time.wait(100)
 
-text_to_speech_with_elevenlabs(input_text, output_filepath="elevenlabs_testing.mp3")
+text_to_speech_with_elevenlabs(input_text, output_filepath="elevenlabs_testing_autoplay.mp3")
